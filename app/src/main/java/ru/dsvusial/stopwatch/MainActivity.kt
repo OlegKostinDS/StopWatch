0916c1cd319fc0144c33da5f7dc7b6960a00bab5
Oleg <olegkostinds@gmail.com>
2022-07-19 17:27:21 +0300

    Initial commit

---
+++ app/src/main/java/ru/dsvusial/stopwatch/MainActivity.kt
@@ -0,0 +1,73 @@
+package ru.dsvusial.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var stopwatch: Chronometer
+    var running = false
+    var offset: Long = 0 // base offset for the stopwatch
+
+    //strings for bundle
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putLong(OFFSET_KEY, offset)
+        outState.putBoolean(RUNNING_KEY, running)
+        outState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(outState)
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running) {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            } else setBaseTime()
+
+        }
+        val startButton = findViewById<Button>(R.id.start)
+        startButton.setOnClickListener {
+            if (!running) {
+                setBaseTime()
+                stopwatch.start()
+                running = true
+            }
+        }
+        val pauseButton = findViewById<Button>(R.id.stop)
+        pauseButton.setOnClickListener {
+            if (running) {
+                saveOffset()
+                stopwatch.stop()
+                running = false
+            }
+        }
+        val resetButton = findViewById<Button>(R.id.reset)
+        resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
+        }
+    }
+
+    private fun setBaseTime() {
+        stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
+    private fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
+    }
+}
